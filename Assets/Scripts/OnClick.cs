1d7de3310c83b99870e11070ff54ca283356f2bc
Dalv-Olan <31648438+Dalv-Olan@users.noreply.github.com>
2017-10-17 16:37:21 +0300

    Added files
    Including the easy to use ab.unitypackage

---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClick : MonoBehaviour {
+
+    public Button ButtonA, ButtonB, next;
+    public Toggle CheckA, CheckB;
+    public AudioClip[] Sounds;
+
+    private AudioSource DasAudio;
+    private int A, B;
+    [SerializeField]
+    public int[] Points;
+
+    // Update is called once per frame
+    void Start () {
+        DasAudio = GetComponent<AudioSource>();
+        ButtonA.onClick.AddListener(ClickA);
+        ButtonB.onClick.AddListener(ClickB);
+        next.onClick.AddListener(NextF);
+
+        Points = new int[Sounds.Length];
+
+        setAB();
+    }
+
+    void ClickA()
+    {
+        DasAudio.clip = Sounds[A];
+        DasAudio.Play();
+    }
+
+    void ClickB()
+    {
+        DasAudio.clip = Sounds[B];
+        DasAudio.Play();
+        Debug.Log("B");
+    }
+
+    void NextF()
+    {
+        if (CheckA.isOn)
+        {
+            Points[A]++;
+        }
+        if (CheckB.isOn)
+        {
+            Points[B]++;
+        }
+
+        CheckA.isOn = false;
+        CheckB.isOn = false;
+
+        GameObject SavEntity = GameObject.Find("Save");
+        SaveFile InterCon2 = SavEntity.GetComponent<SaveFile>();
+
+        InterCon2.Save();
+
+
+        setAB();
+ 
+        
+    }
+
+    void setAB()
+    {
+        A = Random.Range(0, Sounds.Length);
+        B = Random.Range(0, Sounds.Length);
+
+        //always different sounds
+        A = (A==B) ? A = Random.Range(0, Sounds.Length):A;
+
+        Debug.Log(A + "  " + B);
+
+    }
+}
+
---
+++ Assets/Scripts/SaveFile.cs
@@ -0,0 +1,79 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+public class SaveFile : MonoBehaviour {
+
+    private string fileName = "D.txt";
+
+    private void Update()
+    {
+        
+    }
+    void Start () {
+
+        Environment.SetEnvironmentVariable("MONO_REFLECTION_SERIALIZER", "yes");
+        Save();
+
+        if (File.Exists(fileName))
+            {
+                Debug.Log(fileName + " already exists.");
+                return;
+            }
+            var sr = File.CreateText(fileName);
+            sr.WriteLine("Save");
+            sr.WriteLine("file.",
+                1, 4.2);
+            sr.Close();
+        }
+	
+
+    public void Save()
+    {
+        BinaryFormatter LeFormateur = new BinaryFormatter();
+        FileStream LeFichier = File.Create(Application.persistentDataPath + "/sav2.txt") ;
+
+        LeData data = new LeData();
+
+        GameObject Omni = GameObject.Find("Omni");
+        OnClick InterCon = Omni.GetComponent<OnClick>();
+
+        var sr = File.CreateText(fileName);
+        for (int i = 0; (i < InterCon.Sounds.Length); i++)
+         {
+
+            data.Texto = data.Texto + InterCon.Sounds[i] + " | " + InterCon.Points[i] + " @ ";
+            
+            sr.WriteLine(InterCon.Sounds[i] + " | " + InterCon.Points[i]);
+
+        }
+        sr.Close();
+
+        
+        LeFormateur.Serialize (LeFichier,data);
+        LeFichier.Close();
+    }
+
+    public void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/sav.dat"))
+        {
+            BinaryFormatter LeFormateur = new BinaryFormatter();
+            FileStream LeFichier = File.Open(Application.persistentDataPath + "/save.dat", FileMode.Open);
+            //LeData data = (LeData)LeFormateur.Deserialize(LeFichier);
+            //LeFichier.Close();
+            //agility = data.PlayerAGIL;
+        }
+    }
+
+    [Serializable]
+    class LeData
+    {
+        public String Texto;
+    }
+}
+
